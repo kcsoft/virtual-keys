bbf24bc5baed3cc57b084d523db0f83e6e64012d
marcbodea <marcbodea@gmail.com>
2024-09-15 17:02:28 +0200

    added date/time expiration

--- custom_components/virtual_keys/__init__.py
+++ custom_components/virtual_keys/__init__.py
@@ -7,6 +7,7 @@ from homeassistant.auth.models import TOKEN_TYPE_LONG_LIVED_ACCESS_TOKEN
 from homeassistant.helpers.typing import ConfigType
 from homeassistant.components import websocket_api
 from homeassistant.util import dt as dt_util
+from datetime import datetime
 
 DOMAIN = "virtual_keys"
 
@@ -61,13 +62,13 @@ async def list_users(
 
     connection.send_result(msg["id"], result)
 
-
 @websocket_api.websocket_command(
     {
         vol.Required("type"): "virtual_keys/create_token",
         vol.Required("user_id"): str,
-        vol.Required("name"): str, # token name
-        vol.Required("minutes"): int, # minutes
+        vol.Required("name"): str,  # token name
+        vol.Optional("minutes"): int,  # minutes
+        vol.Optional("expiration"): str,  # expiration date and time in ISO format
     }
 )
 @websocket_api.require_admin
@@ -85,11 +86,20 @@ async def create_token(
         return
 
     try:
+        expiration = None
+        if "minutes" in msg:
+            expiration = timedelta(minutes=msg["minutes"])
+        elif "expiration" in msg:
+            expiration = datetime.fromisoformat(msg["expiration"]) - datetime.now()
+
+        if expiration is None:
+            raise ValueError("Either 'minutes' or 'expiration' must be provided")
+
         refresh_token = await hass.auth.async_create_refresh_token(
             user,
             client_name=msg.get("name"),
             token_type=TOKEN_TYPE_LONG_LIVED_ACCESS_TOKEN,
-            access_token_expiration=timedelta(minutes=msg["minutes"]),
+            access_token_expiration=expiration,
         )
         access_token = hass.auth.async_create_access_token(refresh_token)
     except ValueError as err:
@@ -99,7 +109,6 @@ async def create_token(
         return
 
     connection.send_result(msg["id"], access_token)
-
 @websocket_api.websocket_command(
     {
         vol.Required("type"): "virtual_keys/delete_token",
--- dist/virtual-keys.js
+++ dist/virtual-keys.js
@@ -33,40 +33,48 @@ class VirtualKeysPanel extends LitElement {
     super();
     this.users = [];
     this.tokens = [];
-    this.alert = '';
+    this.alert = "";
 
     // form inputs
-    this.name = '';
-    this.user = '';
+    this.name = "";
+    this.user = "";
     this.expire = 60;
+    this.expirationDateTime = "";
   }
 
   fetchUsers() {
-    this.hass.callWS({ type: 'virtual_keys/list_users' }).then(users => {
+    this.hass.callWS({ type: "virtual_keys/list_users" }).then((users) => {
       this.users = [];
       this.tokens = [];
-      users.filter(user => !user.system_generated && user.is_active).forEach(user => {
-        this.users.push({
-          id: user.id,
-          name: user.name,
-        });
-        user.tokens.filter(token => token.type === 'long_lived_access_token' && token.expiration !== 315360000)
-          .forEach(token => {
-            this.tokens.push({
-              id: token.id,
-              name: token.name,
-              user: user.name,
-              jwt_token: token.jwt_token,
-              expiration: token.expiration,
-              remaining: token.remaining,
-            });
+      users
+        .filter((user) => !user.system_generated && user.is_active)
+        .forEach((user) => {
+          this.users.push({
+            id: user.id,
+            name: user.name,
           });
-      });
+          user.tokens
+            .filter(
+              (token) =>
+                token.type === "long_lived_access_token" &&
+                token.expiration !== 315360000
+            )
+            .forEach((token) => {
+              this.tokens.push({
+                id: token.id,
+                name: token.name,
+                user: user.name,
+                jwt_token: token.jwt_token,
+                expiration: token.expiration,
+                remaining: token.remaining,
+              });
+            });
+        });
     });
   }
 
   update(changedProperties) {
-    if (changedProperties.has('hass') && this.hass) {
+    if (changedProperties.has("hass") && this.hass) {
       this.fetchUsers();
     }
     super.update(changedProperties);
@@ -84,56 +92,91 @@ class VirtualKeysPanel extends LitElement {
     this.expire = e.target.value;
   }
 
+  expirationDateTimeChanged(e) {
+    this.expirationDateTime = e.target.value;
+  }
+
   toggleSideBar() {
-    this.dispatchEvent(new Event('hass-toggle-menu', { bubbles: true, composed: true}));
+    this.dispatchEvent(
+      new Event("hass-toggle-menu", { bubbles: true, composed: true })
+    );
   }
 
   addClick() {
-    this.hass.callWS({
-      type: 'virtual_keys/create_token',
+    const msg = {
+      type: "virtual_keys/create_token",
       name: this.name,
       user_id: this.user,
-      minutes: parseInt(this.expire, 10),
-    }).then(() => {
-      this.fetchUsers();
-    }).catch(err => {
-      this.showAlert(err.message);
-    });
+    };
+
+    if (this.expirationDateTime) {
+      msg.expiration = this.expirationDateTime;
+    } else {
+      msg.minutes = parseInt(this.expire, 10);
+    }
+
+    this.hass
+      .callWS(msg)
+      .then(() => {
+        this.fetchUsers();
+      })
+      .catch((err) => {
+        this.showAlert(err.message);
+      });
   }
 
   deleteButton() {
-    return html`<svg preserveAspectRatio="xMidYMid meet" focusable="false" role="img" aria-hidden="true" viewBox="0 0 24 24" width="24" height="24">
-        <g><path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z"></path></g>
-      </svg>`;
+    return html`<svg
+      preserveAspectRatio="xMidYMid meet"
+      focusable="false"
+      role="img"
+      aria-hidden="true"
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+    >
+      <g>
+        <path
+          d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z"
+        ></path>
+      </g>
+    </svg>`;
   }
 
   showAlert(text) {
     this.alert = text;
     setTimeout(() => {
-      this.alert = '';
+      this.alert = "";
     }, 2000);
   }
 
   deleteClick(e, token) {
     e.stopPropagation();
 
-    this.hass.callWS({
-      type: 'virtual_keys/delete_token',
-      token_id: token.id,
-    }).then(() => {
-      this.fetchUsers();
-    }).catch(err => {
-      this.showAlert(err.message);
-    });
+    this.hass
+      .callWS({
+        type: "virtual_keys/delete_token",
+        token_id: token.id,
+      })
+      .then(() => {
+        this.fetchUsers();
+      })
+      .catch((err) => {
+        this.showAlert(err.message);
+      });
   }
 
   getLoginUrl(token) {
-    return this.hass.hassUrl() + 'local/community/virtual-keys/login.html?token=' + token.jwt_token;
+    return (
+      this.hass.hassUrl() +
+      "local/community/virtual-keys/login.html?token=" +
+      token.jwt_token
+    );
   }
 
   listItemClick(e, token) {
     navigator.clipboard.writeText(this.getLoginUrl(token));
-    this.showAlert('Copied to clipboard ' + token.name);
+    this.showAlert("Copied to clipboard " + token.name);
   }
 
   render() {
@@ -143,7 +186,9 @@ class VirtualKeysPanel extends LitElement {
           <div class="mdc-top-app-bar__row">
             <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start" id="navigation">
               <div>
-                <mwc-icon-button title="Sidebar Toggle" @click=${this.toggleSideBar}>
+                <mwc-icon-button title="Sidebar Toggle" @click=${
+                  this.toggleSideBar
+                }>
                   <svg preserveAspectRatio="xMidYMid meet" focusable="false" role="img" aria-hidden="true" viewBox="0 0 24 24">
                     <g><path class="primary-path" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z"></path></g>
                   </svg>
@@ -162,37 +207,60 @@ class VirtualKeysPanel extends LitElement {
 
         <div class="mdc-top-app-bar--fixed-adjust flex content">
           <div class="filters">
-            <ha-textfield label="Key name" value="" @input="${this.nameChanged}"></ha-textfield>
+            <ha-textfield label="Key name" value="" @input="${
+              this.nameChanged
+            }"></ha-textfield>
 
             <ha-combo-box
               .items=${this.users}
-              .itemLabelPath=${'name'}
-              .itemValuePath=${'id'}
+              .itemLabelPath=${"name"}
+              .itemValuePath=${"id"}
               .value="1"
-              .label=${'User'}
+              .label=${"User"}
               @value-changed=${this.userChanged}
             >
             </ha-combo-box>
 
-            <ha-textfield label="Expire (minutes)" type="number" value="${this.expire}"" @input="${this.expireChanged}"></ha-textfield>
-
+            <ha-textfield label="Expire (minutes)" type="number" value="${
+              this.expire
+            }"" @input="${this.expireChanged}"></ha-textfield>
+            
+            <ha-textfield 
+              label="Expiration Date and Time (optional)" 
+              type="datetime-local" 
+              value="${this.expirationDateTime}" 
+              @input="${this.expirationDateTimeChanged}">
+            </ha-textfield>
             <mwc-button raised label="Add" @click=${this.addClick}></mwc-button>
           </div>
 
           <ha-card>
             <mwc-list>
-              ${this.tokens.map(token => html`
-                <mwc-list-item hasMeta twoline @click=${e => this.listItemClick(e, token)}>
-                  <a href="${this.getLoginUrl(token)}">${token.name}</a>
-                  <span slot="secondary">${token.user}, Expire: ${humanSeconds(token.remaining)}</span>
-                  <mwc-icon slot="meta" @click=${e => this.deleteClick(e, token)}>${this.deleteButton()}</mwc-icon>
-                </mwc-list-item>
-              `)}
+              ${this.tokens.map(
+                (token) => html`
+                  <mwc-list-item
+                    hasMeta
+                    twoline
+                    @click=${(e) => this.listItemClick(e, token)}
+                  >
+                    <a href="${this.getLoginUrl(token)}">${token.name}</a>
+                    <span slot="secondary"
+                      >${token.user}, Expire:
+                      ${humanSeconds(token.remaining)}</span
+                    >
+                    <mwc-icon
+                      slot="meta"
+                      @click=${(e) => this.deleteClick(e, token)}
+                      >${this.deleteButton()}</mwc-icon
+                    >
+                  </mwc-list-item>
+                `
+              )}
             </mwc-list>
           </ha-card>
         </div>
 
-      ${this.alert.length ? html`<ha-alert>${this.alert}</ha-alert>` : ''}
+      ${this.alert.length ? html`<ha-alert>${this.alert}</ha-alert>` : ""}
 
       </div>
     `;
@@ -204,9 +272,12 @@ class VirtualKeysPanel extends LitElement {
       }
       .mdc-top-app-bar {
         --mdc-typography-headline6-font-weight: 400;
-        color: var(--app-header-text-color,var(--mdc-theme-on-primary,#fff));
-        background-color: var(--app-header-background-color,var(--mdc-theme-primary));
-        width: var(--mdc-top-app-bar-width,100%);
+        color: var(--app-header-text-color, var(--mdc-theme-on-primary, #fff));
+        background-color: var(
+          --app-header-background-color,
+          var(--mdc-theme-primary)
+        );
+        width: var(--mdc-top-app-bar-width, 100%);
         display: flex;
         position: fixed;
         flex-direction: column;
@@ -244,13 +315,22 @@ class VirtualKeysPanel extends LitElement {
       }
       .mdc-top-app-bar__title {
         -webkit-font-smoothing: antialiased;
-        font-family: var(--mdc-typography-headline6-font-family,var(--mdc-typography-font-family,Roboto,sans-serif));
-        font-size: var(--mdc-typography-headline6-font-size,1.25rem);
-        line-height: var(--mdc-typography-headline6-line-height,2rem);
-        font-weight: var(--mdc-typography-headline6-font-weight,500);
-        letter-spacing: var(--mdc-typography-headline6-letter-spacing,.0125em);
-        text-decoration: var(--mdc-typography-headline6-text-decoration,inherit);
-        text-transform: var(--mdc-typography-headline6-text-transform,inherit);
+        font-family: var(
+          --mdc-typography-headline6-font-family,
+          var(--mdc-typography-font-family, Roboto, sans-serif)
+        );
+        font-size: var(--mdc-typography-headline6-font-size, 1.25rem);
+        line-height: var(--mdc-typography-headline6-line-height, 2rem);
+        font-weight: var(--mdc-typography-headline6-font-weight, 500);
+        letter-spacing: var(
+          --mdc-typography-headline6-letter-spacing,
+          0.0125em
+        );
+        text-decoration: var(
+          --mdc-typography-headline6-text-decoration,
+          inherit
+        );
+        text-transform: var(--mdc-typography-headline6-text-transform, inherit);
         padding-left: 20px;
         padding-right: 0px;
         text-overflow: ellipsis;
@@ -268,7 +348,7 @@ class VirtualKeysPanel extends LitElement {
         height: var(--header-height);
       }
       app-toolbar [main-title] {
-        margin-left: 20px
+        margin-left: 20px;
       }
       ha-combo-box {
         padding: 8px 0;
